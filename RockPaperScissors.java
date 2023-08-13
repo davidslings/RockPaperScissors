48ca98c57984ce2f849d16b61652dd034ea88115
davidslings <david.slings@calco.nl>
2023-08-13 17:13:16 +0200

    removed some bugs

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -2,67 +2,66 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
         int winCount = 0;
         int loseCount = 0;
         int tieCount = 0;
         System.out.println("Let's play Rock Paper Scissors.");
         System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.\n");
-        playRockPaperScissors(winCount, loseCount, tieCount);
-
-    }
-
-    // Task 1: See if the user wants to play.
-    public static void playRockPaperScissors(int winCount, int loseCount, int tieCount) {
-        Scanner scan = new Scanner(System.in);
         System.out.println("Are you ready? Write 'yes' if you are.");
         String answer = scan.nextLine();
-
-        /*
-         * Task 2: Set up the game
-         */
         if (answer.equals("yes")) {
             System.out.println("Great!\n");
-            System.out.println("1. rock - 2. paper - 3. scissors, shoot!");
-            int yourChoice;
-            try {
-                yourChoice = scan.nextInt();
-                if (yourChoice > 3 || yourChoice < 1) {
-                    System.out.println("Invalid input: enter a number between 1 and 3.");
-                    scan.close();
-                    return;
-                }
-            } catch (Exception e) {
+            playRockPaperScissors(winCount, loseCount, tieCount);
+        } else {
+            System.out.println("\nDarn, some other time");
+            scan.close();
+            return;
+        }
+        scan.close();
+    }
+
+
+    public static void playRockPaperScissors(int winCount, int loseCount, int tieCount) {
+        Scanner scan = new Scanner(System.in);        
+        System.out.println("1. rock - 2. paper - 3. scissors, shoot!");
+        int yourChoice;
+        try {
+            yourChoice = scan.nextInt();
+            if (yourChoice > 3 || yourChoice < 1) {
                 System.out.println("Invalid input: enter a number between 1 and 3.");
                 scan.close();
                 return;
             }
-            int compChoice = computerChoice();
-            int result = getResult(yourChoice, compChoice);
-            System.out.println("\nYou chose:     " + yourChoice);
-            System.out.println("Computer chose: " + compChoice);
-            if (result == -1) {
-                System.out.println("\nYou lose");
-                loseCount++;
-                System.out.println("\nWins: " + winCount);
-                System.out.println("Losses: " + loseCount);
-                System.out.println("Ties " + tieCount);
-            } else if (result == 0) {
-                System.out.println("You're tied");
-                tieCount++;
-                System.out.println("Wins: " + winCount);
-                System.out.println("Losses: " + loseCount);
-                System.out.println("Ties " + tieCount);
-            } else {
-                System.out.println("You win!");
-                winCount++;
-                System.out.println("Wins: " + winCount);
-                System.out.println("Losses: " + loseCount);
-                System.out.println("Ties " + tieCount);
-            }
-        } else {
-            System.out.println("\nDarn, some other time");
+        } catch (Exception e) {
+            System.out.println("Invalid input: enter a number between 1 and 3.");
+            scan.close();
+            return;
         }
+        int compChoice = computerChoice();
+        int result = getResult(yourChoice, compChoice);
+        System.out.println("\nYou chose:     " + yourChoice);
+        System.out.println("Computer chose: " + compChoice);
+        if (result == -1) {
+            System.out.println("\nYou lose");
+            loseCount++;
+            System.out.println("\nWins: " + winCount);
+            System.out.println("Losses: " + loseCount);
+            System.out.println("Ties " + tieCount);
+        } else if (result == 0) {
+            System.out.println("\nYou're tied");
+            tieCount++;
+            System.out.println("Wins: " + winCount);
+            System.out.println("Losses: " + loseCount);
+            System.out.println("Ties " + tieCount);
+        } else {
+            System.out.println("\nYou win!");
+            winCount++;
+            System.out.println("\nWins: " + winCount);
+            System.out.println("Losses: " + loseCount);
+            System.out.println("Ties " + tieCount);
+        }
+        
 
         System.out.println("\nDo you want to play another game?");
         scan.nextLine();
@@ -71,10 +70,10 @@ public class RockPaperScissors {
             playRockPaperScissors(winCount, loseCount, tieCount);
         } else {
             System.out.println("Ok, goodbye.");
-            System.exit(0);
         }
+        
         scan.close();
-
+        return;
     }
     // Task 3 – Write a function where the computer picks a random choice.
 
